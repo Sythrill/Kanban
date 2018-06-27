32020f9df8a52aba392b352fed2a930a4155a298
Sythrill <ksciwiarska@gmail.com>
2018-06-27 20:45:31 +0200

    modify lane reducer add edit and delete note actions

--- client/modules/Lane/LaneReducer.js
+++ client/modules/Lane/LaneReducer.js
@@ -1,4 +1,5 @@
 import {CREATE_LANE, UPDATE_LANE, DELETE_LANE} from './LaneActions';
+import {CREATE_NOTE, DELETE_NOTE} from '../Note/NoteActions';
 
 const initialState = [];
 
@@ -18,6 +19,24 @@ export default function lanes(state = initialState, action) {
 
     case DELETE_LANE:
       return state.filter(lane => lane.id !== action.laneId);
+
+    case CREATE_NOTE:
+      return state.map(lane => {
+        if (lane.id === action.laneId) {
+          const notes = [...lane.notes, action.note.id];
+          return {...lane, notes};
+        }
+        return lane;
+      });
+
+    case DELETE_NOTE:
+      return state.map(lane => {
+        if (lane.id === action.laneId) {
+          const notes = [...lane.notes, action.note.id];
+          return notes.filter((note) => note.id !== action.noteId);
+        }
+        return lane;
+      });
 
     default:
       return state;
